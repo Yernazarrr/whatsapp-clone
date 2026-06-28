c6ac8b999e4d46ee5a1e9568d6ca2796b5adb427
Yernazarrr <yernazar.mukametali@gmail.com>
2026-06-28 07:01:15 +0500

    added my message card

---
+++ lib/widgets/my_message_card.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import '../core/theme/app_colors.dart';
+
+class MyMessageCard extends StatelessWidget {
+  final String message;
+  final String date;
+
+  const MyMessageCard({
+    super.key,
+    required this.message,
+    required this.date,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: .centerRight,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width - 45,
+        ),
+        child: Card(
+          elevation: 1,
+          shape: RoundedRectangleBorder(
+            borderRadius: .circular(8),
+          ),
+          color: AppColors.messageColor,
+          margin: const .symmetric(horizontal: 15, vertical: 5),
+          child: Stack(
+            children: [
+              Padding(
+                padding: const .only(
+                  left: 10,
+                  right: 30,
+                  top: 5,
+                  bottom: 20,
+                ),
+                child: Text(
+                  message,
+                  style: const TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 4,
+                right: 10,
+                child: Row(
+                  children: [
+                    Text(
+                      date,
+                      style: const TextStyle(
+                        fontSize: 13,
+                        color: Colors.white60,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    const Icon(
+                      Icons.done_all,
+                      size: 20,
+                      color: Colors.white60,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
